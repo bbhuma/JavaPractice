f3785cb687b789f1c58be978fe96ebb2fdfa47e8
bgrbhuma@gmail.com <bgrbhuma@gmail.com>
2026-02-20 10:21:32 +0530

    Bit manipulation session 30

--- src/main/java/com/java/JavaRefresherIntermediate/Sessionn30/FindNthMagicNumber.java
+++ src/main/java/com/java/JavaRefresherIntermediate/Sessionn30/FindNthMagicNumber.java
@@ -3,10 +3,24 @@ package com.java.JavaRefresherIntermediate.Sessionn30;
 public class FindNthMagicNumber {
 
 	public static void main(String[] args) {
-
+		int ans = findNthMagicNumber(10);
+		System.out.println(ans);
+		System.out.println(findNthMagicNumber(5));
 	}
-	public static void findNthMagicNumber(String[] args) {
 
+	// unique powers of 5 sum, ath element. 
+	public static int findNthMagicNumber(int num) {
+		int power = 5;
+		int magicNumber = 0;
+		while(num>0) {
+			if((num & 1) ==1 ) {
+				magicNumber += power;
+			}
+			power *= 5;
+			num >>=1;
+		}
+		
+		return magicNumber;
 	}
 
 }
