30037fe9df547cb62b3d765176859a43b6dcbbce
bgrbhuma@gmail.com <bgrbhuma@gmail.com>
2026-06-28 22:05:26 +0530

    session 34

--- src/main/java/com/java/JavaRefresherIntermediate/Sessionn34/GreatestCommonDivisor.java
+++ src/main/java/com/java/JavaRefresherIntermediate/Sessionn34/GreatestCommonDivisor.java
@@ -3,8 +3,25 @@ package com.java.JavaRefresherIntermediate.Sessionn34;
 public class GreatestCommonDivisor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(getGreatestCommonDivisor(25, 15));
+		System.out.println(getGreatestCommonDivisor(12, 30));
+		System.out.println(getGreatestCommonDivisor(9, 18));
+		System.out.println(getGreatestCommonDivisor(81, 153));
 
 	}
+
+	public static int getGreatestCommonDivisor(int m, int n) {
+		if (n==0 ) {
+			return m;
+		}
+		if (m==0) {
+			return n;
+		}
+		if(m<n) {
+			return getGreatestCommonDivisor(n%m, m);
+		}else 
+		return getGreatestCommonDivisor(m % n, n);
+	}
+	
 
 }
